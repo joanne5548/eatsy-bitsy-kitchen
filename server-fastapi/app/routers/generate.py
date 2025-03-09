e8390ffa5ec3c0cd3e0c4070e380cc859417b767
joanne5548 <joanne.kim0328@gmail.com>
2025-03-08 19:21:26 -0500

    fixed the redirect errorgit add .

--- server-fastapi/app/routers/generate.py
+++ server-fastapi/app/routers/generate.py
@@ -4,6 +4,7 @@ from app.openai_api.openai_client import generate_recipe
 
 router = APIRouter(prefix='/api/generate', tags=['generate'])
 
+# Note that this works only for endpoint "http://localhost:8000/api/generate/" WITH trailing slash!!
 @router.post('/')
 async def generate(options: InputOptions) -> Recipe:
     recipe = generate_recipe(options)
